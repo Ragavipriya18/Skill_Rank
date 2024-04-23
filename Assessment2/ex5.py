c8098a3ce3db44d4984dc51c354692a46ca4354c
Ragavi <129532617+Ragavipriya18@users.noreply.github.com>
2024-04-23 20:03:56 +0530

    Assessment 2 completed

---
+++ Assessment2/ex5.py
@@ -0,0 +1,11 @@
+import json
+with open('ex5.json') as f:
+
+	ex5 = json.load(f)
+for item in ex5:
+	if item['name'] == 'Old Fashioned':
+		item['batters']['batter'].append({"id" : "1003", "type" : "Tea"})
+		break
+
+with open('ex5.json', 'w') as f:
+	json.dump(ex5,f,indent=4)
